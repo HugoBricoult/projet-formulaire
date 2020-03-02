75a0b7188ae7e28d3824ace6dcfec52da73759a9
Rahino <54941830+estebanisto@users.noreply.github.com>
2020-03-02 14:20:01 +0100

    qfg

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light ">
   <a class="navbar-brand" href="index.php"><img id="logo" src="img/logo.png" alt="Logo hackers poulette"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
