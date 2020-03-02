6ec8dd877d432b083d4bf6c60c40d5ede16e6407
Rahino <54941830+estebanisto@users.noreply.github.com>
2020-03-02 16:34:49 +0100

    fin de journée

--- pages/articles.php
+++ pages/articles.php
@@ -8,3 +8,61 @@
 </p>
   <a class="btn btn-primary btn-lg" href="#" role="button">Raspberry Pi 4</a>
 </section>
+
+<!-- Présentation -->
+<article class="article-intro">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6 intro-cel">
+                <img src="./img/rasp-ar.jpg" alt="image de respberry">
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 intro-text intro-cel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti odit 
+                    iusto nesciunt sapiente quia mollitia doloribus nemo cumque voluptatem 
+                    at debitis, vitae dolor laudantium, ducimus adipisci, quo dignissimos 
+                    labore velit!<br><br>
+                    <a href="https://fr.wikipedia.org/wiki/Raspberry_Pi"><i class="fa fa-wikipedia-w" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></p>
+            </div>
+        </div>
+    </div>
+</article>
+
+<hr id="interligne">
+
+<!--CARD-->
+<section class="card">
+    <div class="container">
+        <div class="card-deck">
+            <div class="card">
+                <img class="card-img-top" src="./img/model.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Model</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="./img/accessoirs.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Accessoires</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="./img/promo.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Promo</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="./img/top.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Top</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
