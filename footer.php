bd59481e571d79ada52b334d9f3409f08879d2d6
hugo <hugo.bricoult0521@gmail.com>
2020-03-02 15:51:15 +0100

    check

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+            <div class="col-12"></div>
+        </div>
+    </div>
+</footer>
--- pages/articles.php
+++ pages/articles.php
@@ -1,8 +1,60 @@
 <!--jumbotron-->
-<section class="jumbotron jumbotron-img">
-  <h1 class="display-4">Hello, world!</h1>
+<section class="jumbotron jumbotron-img text-center">
+  <h1 class="display-4">Hackers Poulette</h1>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
   <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</section>
+<article class="article-intro">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6 intro-cel">
+                <img src="./img/rasp-ar.jpg" alt="image de respberry">
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 intro-text intro-cel">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti odit 
+                    iusto nesciunt sapiente quia mollitia doloribus nemo cumque voluptatem 
+                    at debitis, vitae dolor laudantium, ducimus adipisci, quo dignissimos 
+                    labore velit!<br><br>
+                    <i class="fa fa-wikipedia-w" aria-hidden="true"></i>
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                    <i class="fa fa-instagram" aria-hidden="true"></i></p>
+            </div>
+        </div>
+    </div>
+</article>
+<section class="card">
+    <div class="container">
+        <div class="card-deck">
+            <div class="card">
+                <img class="card-img-top" src="./img/model.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Model</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="./img/accessoirs.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Accessoires</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="./img/promo.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Promo</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="./img/top.jpg" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Top</h4>
+                    <p class="card-text">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
