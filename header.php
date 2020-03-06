76204ee261574a2ae62b27738f53925d733bc69b
hugo <hugo.bricoult0521@gmail.com>
2020-03-06 09:57:27 +0100

    sav

--- header.php
+++ header.php
@@ -31,3 +31,34 @@
     </ul>
   </div>
 </nav>
+<nav class="navbar navbar-expand-sm navbar-light bg-light navbar-expand-lg  p-4">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+      aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavId">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown ">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Raspberry</a>
+        <div class="dropdown-menu" aria-labelledby="dropdownId">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Promo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Top vente</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?page=support">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
